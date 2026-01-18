9939fed763cf2cc116de1cc0610b282537473d8f
Lê Quang Anh <lequanganhh1624@gmail.com>
2026-01-19 01:17:15 +0700

    update file

--- src/main/java/com/rikkei/reshoppingcartwebapp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rikkei.reshoppingcartwebapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rikkei.reshoppingcartwebapp.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
